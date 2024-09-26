d4e255526f26719fe2a9634ea1cdb68b907493f1
Pedro Colombano <pedromaiacolombano@gmail.com>
2024-09-26 15:39:52 -0300

    test(api-gateway): add application context test

--- api-gateway/src/test/java/br/com/itau/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/br/com/itau/apigateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.itau.apigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class ApiGatewayApplicationTests {
 
     @Test
