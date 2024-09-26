79a6bc48793354d4679c353081ee585b2c54bb27
Pedro Colombano <pedromaiacolombano@gmail.com>
2024-09-26 15:42:10 -0300

    feat(account-ms): create project

---
+++ account-ms/src/test/java/br/com/itau/accountms/AccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.accountms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+class AccountMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
