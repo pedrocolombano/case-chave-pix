4e033ca3cf40d5346c524603b3b22ae6053b7068
Pedro Colombano <pedromaiacolombano@gmail.com>
2024-09-26 15:20:03 -0300

    feat(api-gateway): implement gateway for microservices

---
+++ api-gateway/src/main/java/br/com/itau/apigateway/config/RouteConfiguration.java
@@ -0,0 +1,20 @@
+package br.com.itau.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfiguration {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/users/**")
+                                                .filters(f -> f.prefixPath("/user-service"))
+                                                .uri("lb://user-ms"))
+                .build();
+    }
+
+}
