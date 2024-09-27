dcf28310f4c4c4fc447f2f736453d63b867d45bc
Pedro Colombano <pedromaiacolombano@gmail.com>
2024-09-27 01:01:51 -0300

    feat(transactional-account-ms): create project

---
+++ transactional-account-ms/src/main/java/br/com/itau/transactionalaccountms/TransactionalAccountMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.transactionalaccountms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalAccountMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalAccountMsApplication.class, args);
+    }
+
+}
---
+++ transactional-account-ms/src/test/java/br/com/itau/transactionalaccountms/TransactionalAccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.transactionalaccountms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalAccountMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
